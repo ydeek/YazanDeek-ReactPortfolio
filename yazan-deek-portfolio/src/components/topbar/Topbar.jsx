d59123243485c056fc33ccbe82046885c7caa4e5
Yazan Deek <ydeek91@yahoo.com>
2022-01-16 16:58:09 -0700

    added icons from material ui

--- yazan-deek-portfolio/src/components/topbar/Topbar.jsx
+++ yazan-deek-portfolio/src/components/topbar/Topbar.jsx
@@ -1,19 +1,20 @@
 import "./topbar.scss"
-
+import from { Person, Mail } from "@materail-ui/icons"
 
 
 function Topbar() {
     return (
         <div className="topbar">
             <div className="wrapper">
-
-
                 <div className="left">
-                    < a href="#intro" className="logo">YAZAN DEEK.</a>
+                    < a href="#intro" className="logo">YAZAN DEEK.</a> </div>
+
+                <div className="itemContainer">
+                    <Person />
+                    <span>+44 425 66 7</span>
                 </div>
 
                 <div className="right"></div>
-
             </div>
         </div>
     )
